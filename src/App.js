0dcbb3f8fd119d422e2c984edec2fe9b4abbc504
Annie Y <annie_y48@hotmail.com>
2017-01-31 14:06:45 -0800

    Added json file and data

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import $ from 'jquery';
 import './App.css';
 
 // Components
@@ -10,7 +11,36 @@ import Portfolio from './Components/Portfolio';
 import Resume from './Components/Resume';
 
 class App extends Component {
+  constructor(props){
+    super(props); // access properties if needed
+    this.state = {
+      foo: 'bar',
+      resumeData:{}
+    }
+  }
+  getResumeData(){
+    $.ajax({
+      url: 'http://localhost:3000/resumeData.json',
+      dataType: 'json',
+      cache: false,
+      // function to run if everything is okay
+      success: function(data){
+        // send resumeData to the data that is being fetched
+        this.setState({resumeData: data});
+      }.bind(this),
+        error:function(xhr, status, err){ // if something goes wrong, run this function
+          console.log(err);
+          alert(err);
+        }
+    });
+  }
+
+  ComponentDidMount(){
+    this.getResumeData();
+  }
+
   render() {
+    console.log(this.state.resumeData);
     return (
       <div className="App">
         <Header/>
